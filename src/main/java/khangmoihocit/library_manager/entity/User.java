4e75d916cc2cbe0691a617d01bc35af4c77771b6
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-06-26 17:37:05 +0700

    first commit

---
+++ src/main/java/khangmoihocit/library_manager/entity/User.java
@@ -0,0 +1,46 @@
+package khangmoihocit.library_manager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    String username;
+
+    @Column(name="password", nullable = false)
+    String password;
+
+    @Column(name = "email", unique = true)
+    String email;
+
+    @Column(name = "full_name")
+    String fullName;
+
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_name"))
+    Set<Role> roles = new HashSet<>();
+
+}
---
+++ src/test/java/khangmoihocit/library_manager/LibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package khangmoihocit.library_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
