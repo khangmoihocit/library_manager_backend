2941e29ad294c213adc37c1664ea1b38cfaba9e4
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-12-19 20:18:30 +0700

    config database seeder

--- src/main/java/com/khangmoihocit/learn/databases/seeder/DatabaseSeeder.java
+++ src/main/java/com/khangmoihocit/learn/databases/seeder/DatabaseSeeder.java
@@ -8,28 +8,35 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-
+//init database
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public DatabaseSeeder(){
-        this.passwordEncoder = new BCryptPasswordEncoder(10);
-    }
-
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("something seeding ...");
+        if(isTableEmpty()){
+            String passwordEncode = passwordEncoder.encode("password");
+            entityManager.createNativeQuery("insert into users(user_catalogue_id, name, email, password, phone) values (?, ?, ?, ?, ?)")
+                    .setParameter(1, 1)
+                    .setParameter(3, "khang567.ht@gmail.com")
+                    .setParameter(2, "Pham Van Khang")
+                    .setParameter(4, passwordEncode)
+                    .setParameter(5, "09876652")
+                    .executeUpdate();
+        }
     }
 
     private boolean isTableEmpty(){
-        Long count = (Long) entityManager.createQuery("select count(id) from users", User)
-                .getSingleResult();
-        return true;
+        Long count = (Long) entityManager.createQuery("select count(id) from User").getSingleResult();
+        return count == 0;
     }
 }
--- src/main/java/com/khangmoihocit/learn/modules/users/entities/User.java
+++ src/main/java/com/khangmoihocit/learn/modules/users/entities/User.java
@@ -1,4 +1,45 @@
 package com.khangmoihocit.learn.modules.users.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.Generated;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    Long user_catalogue_id;
+    String name;
+    String email;
+    String password;
+    String phone;
+    String image;
+    String address;
+
+    @Column(name = "create_at")
+    LocalDateTime createAt;
+    @Column(name = "update_at")
+    LocalDateTime updateAt;
+
+    @PrePersist
+    protected void onCreated(){
+        createAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdated(){
+        updateAt = LocalDateTime.now();
+    }
 }
