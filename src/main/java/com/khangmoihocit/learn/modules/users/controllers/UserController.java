64e42a590261ffb5b1c10c263f0dcc35ac0aafed
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-12-28 15:22:29 +0700

    end verify token

--- src/main/java/com/khangmoihocit/learn/modules/users/controllers/UserController.java
+++ src/main/java/com/khangmoihocit/learn/modules/users/controllers/UserController.java
@@ -8,7 +8,10 @@ import com.sun.net.httpserver.Authenticator;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
+@Slf4j(topic = "USER CONTROLLER")
 public class UserController {
 
     UserRepository userRepository;
 
     @GetMapping("/my-profile")
     ResponseEntity<?> me(){
-        String email = "khang567.ht@gmail.com";
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info(email);
+
         User user = userRepository.findByEmail(email).orElseThrow(()->new RuntimeException("User không tồn tại"));
         UserResource userResource = UserResource.builder()
                 .id(user.getId())
                 .name(user.getName())
                 .email(user.getEmail())
+                .phone(user.getPhone())
                 .build();
 
         SuccessResource<UserResource> response = SuccessResource.<UserResource>builder()
